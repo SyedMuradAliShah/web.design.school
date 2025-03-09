521c1bbecdc3ec8f532baebe5558a9e038da30b4
sulaiman-khan <sallokhan01999@gmail.com>
2025-03-09 15:07:28 +0500

    change home page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,29 +1,23 @@
 if (document.querySelector(".mySwiper")) {
   var swiper = new Swiper(".mySwiper", {
-    autoplay: true,
+    // autoplay: true,
     loop: true,
   });
 }
 
 // testmonails
-if (document.querySelector(".myGrade")) {
-  var swiper = new Swiper(".myGrade", {
+if (document.querySelector(".myTeachers")) {
+  var swiper = new Swiper(".myTeachers", {
     spaceBetween: 20,
-    autoplay: true,
+    // autoplay: true,
     loop: true,
     pagination: false,
     breakpoints: {
       320: {
         slidesPerView: 1,
       },
-      550: {
-        slidesPerView: 2,
-      },
       1024: {
-        slidesPerView: 4,
-      },
-      1280: {
-        slidesPerView: 5,
+        slidesPerView: 2,
       },
     },
   });
